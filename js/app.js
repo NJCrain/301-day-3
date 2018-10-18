abfaafbcb10438483875738dfa8665cc966b7c54
Nicholas Crain <nicholascrain@gmail.com>
2018-10-18 11:23:39 -0700

    added sorting on page load

--- js/app.js
+++ js/app.js
@@ -12,6 +12,30 @@ function Horned(animalObject) {
   objArray.push(this);
 }
 
+function sortByKey(arr) {
+  arr.sort((a, b) => {
+    if (a.keyword > b.keyword) {
+      return 1;
+    }
+    else if (a.keyword < b.keyword) {
+      return -1;
+    }
+    else {return 0;}
+  });
+}
+
+function sortByHorns(arr) {
+  arr.sort((a, b) => {
+    // console.log(a.horns, b.horns);
+    if (parseInt(a.horns) > parseInt(b.horns)) {
+      return 1;
+    }
+    else if (parseInt(a.horns) < parseInt(b.horns)) {
+      return -1;
+    }
+    else {return 0;}
+  });
+}
 
 function renderAnyHandlebars(sourceId, data, target) {
   let template = Handlebars.compile($(sourceId).html());
@@ -35,7 +59,7 @@ addOptionEl();
      data.forEach(obj => {
        new Horned(obj);
      });
-   }).then(renderImages);
+   }).then(() => {sortByKey(objArray)}).then(renderImages);
  }
 
  function resetData() {
@@ -70,16 +94,20 @@ $('select').on('change', function(){
   let $select = $(this).val();
   $('section').hide();
   $(`section[data-keyword="${$select}"]`).show();
-})
+});
 
 //button submit handler
   //delete all main content
-  //re-render with targeted JSON
+  //re-render but sorted
 
 $('button[value="page1"]').on('click', () => {
   readData('page-1');
 });
 $('button[value="page2"]').on('click', () => {
   readData('page-2');
+});
+
+$('button[value="sortKeyword"]').on('click', () => {
+
 });
 
